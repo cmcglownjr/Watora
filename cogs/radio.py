e19dcae224d10e5d365084e37d4824b637479d64
Justin Garringer <49808342+soulnull@users.noreply.github.com>
2024-03-06 15:17:36 -0500

    improve

--- cogs/radio.py
+++ cogs/radio.py
@@ -1,41 +1,37 @@
 import aiohttp
 import asyncio
 import listenmoe
-
-# from monstercatFM import monstercat
-from utils.watora import log
 from discord.ext import commands
-
+# from monstercatFM import monstercat  # Unused import statement removed
+from utils.watora import log
 
 class Radio(commands.Cog):
-    def __init__(self, bot):
+    """A cog for handling radio streams."""
+
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
         self.tasks = []
         self.session = aiohttp.ClientSession(loop=self.bot.loop)
-        asyncio.ensure_future(self.start())
+        asyncio.create_task(self.start())
 
     def cog_unload(self):
         for task in self.tasks:
             task.cancel()
-        asyncio.ensure_future(self.session.close())
+        asyncio.create_task(self.session.close())
 
-    async def players_update(self, kpop=False, mc=False):
-        if 'Music' in self.bot.cogs:  # auto-np radio songs, sweet.
-            music = self.bot.cogs['Music']
+    async def players_update(self, kpop: bool = False, mc: bool = False):
+        """Update all players."""
+        try:
             if not mc:
-                try:
-                    await music.update_all_listen_moe_players(kpop)
-                except Exception as e:
-                    log.warning(
-                        f"[LISTEN.moe] Updating {'K-POP' if kpop else 'J-POP'} players failed with error : {e}")
+                await self.bot.cogs['Music'].update_all_listen_moe_players(kpop)
             else:
-                try:
-                    await music.update_all_mc_players()
-                except Exception as e:
-                    log.warning(
-                        f"[MonsterCat] Updating players failed with error : {e}")
+                # Monstercat is disabled, so this will never be called
+                await self.bot.cogs['Music'].update_all_mc_players()
+        except Exception as e:
+            log.warning(
+                f"[LISTEN.moe] Updating {'K-POP' if kpop else 'J-POP'} players failed with error : {e}")
 
-    async def hand(self, msg):
+    async def hand(self, msg: listenmoe.message.BaseMessage):
         before = self.bot.now
 
         if msg.type == listenmoe.message.SONG_INFO:
@@ -46,7 +42,7 @@ class Radio(commands.Cog):
         if before != self.bot.now:  # avoid the first useless updates when starting the bot / loading the cog
             await self.players_update()
 
-    async def handkpop(self, msg):
+    async def handkpop(self, msg: listenmoe.message.BaseMessage):
         before = self.bot.nowkpop
 
         if msg.type == listenmoe.message.SONG_INFO:
@@ -57,7 +53,7 @@ class Radio(commands.Cog):
         if before != self.bot.nowkpop:  # avoid the first useless updates when starting the bot / loading the cog
             await self.players_update(kpop=True)
 
-    async def mchand(self, msg):
+    async def mchand(self, msg: listenmoe.message.BaseMessage):
         before = self.bot.mcnow
         self.bot.mcnow = msg
 
@@ -65,24 +61,25 @@ class Radio(commands.Cog):
             await self.players_update(mc=True)
 
     async def start(self):
+        """Start the cog."""
         await self.bot.wait_until_ready()
 
         kp = listenmoe.client.Client(loop=self.bot.loop, kpop=True)
         kp.register_handler(self.handkpop)
-        task = asyncio.ensure_future(kp.start())
+        task = asyncio.create_task(kp.start())
         self.tasks.append(task)
 
         cl = listenmoe.client.Client(loop=self.bot.loop)
         cl.register_handler(self.hand)
-        task = asyncio.ensure_future(cl.start())
+        task = asyncio.create_task(cl.start())
         self.tasks.append(task)
 
-        # Monstercat disabled since a while..
+        # Monstercat is disabled, so this is commented out
         # mc = monstercat.Client(loop=self.bot.loop, aiosession=self.session)
         # mc.register_handler(self.mchand)
-        # task = asyncio.ensure_future(mc.start())
+        # task = asyncio.create_task(mc.start())
         # self.tasks.append(task)
 
-
-def setup(bot):
-    bot.add_cog(Radio(bot))
+async def setup(bot: commands.Bot):
+    """Create and add the cog to the bot."""
+    await bot.add_cog(Radio(bot))
