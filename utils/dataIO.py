f65c8f1d2234dc17130e5192338047676ef79ad3
Justin Garringer <49808342+soulnull@users.noreply.github.com>
2024-03-06 15:43:24 -0500

    Revert "improve"
    This reverts commit 74bf8130a507d8da429deea0f1b4f26f7785596f.

--- utils/dataIO.py
+++ utils/dataIO.py
@@ -1,47 +1,42 @@
 import json
 import os
-from typing import Any, Dict, Union
-from typing import Optional
+from random import randrange
+
 
 class InvalidFileIO(Exception):
     pass
 
-class DataIO:
+
+class DataIO():
     def __init__(self):
         pass
 
-    def save_json(self, filename: str, data: Union[Dict[str, Any], list]) -> bool:
-        """Save json file and avoid JSON issues.
-
-        Args:
-            filename (str): The name of the file to save.
-            data (Dict[str, Any] or list): The data to save in json format.
-
-        Returns:
-            bool: True if the file was saved successfully, False otherwise.
-        """
+    def save_json(self, filename, data):
+        """Saves json file and avoid Json issues."""
         rand = randrange(1000, 10000)
         path, ext = os.path.splitext(filename)
-        tmp_file = f"{path}-{rand}.tmp"
-
-        if os.path.exists(filename):
-            os.remove(filename)
-
+        tmp_file = "{}-{}.tmp".format(path, rand)
+        self.really_save_json(tmp_file, data)
         try:
-            self.really_save_json(tmp_file, data)
-            self.validate_json(tmp_file)
-            os.replace(tmp_file, filename)
-            return True
-        except (json.decoder.JSONDecodeError, IOError) as e:
-            print(f"Error saving json file: {e}")
-            self.delete_tmp_file(tmp_file)
+            self.read_json(tmp_file)
+        except json.decoder.JSONDecodeError:
             return False
+        os.replace(tmp_file, filename)
+        return True
 
-    def load_json(self, filename: str) -> Optional[Union[Dict[str, Any], list]]:
-        """Load json file and return the data.
+    def load_json(self, filename):
+        return self.read_json(filename)
 
-        Args:
-            filename (str): The name of the file to load.
+    def read_json(self, filename):
+        with open(filename, encoding='utf-8-sig', mode="r") as f:
+            data = json.load(f)
+        return data
 
-        Returns:
-            Dict[str, Any] or list
+    def really_save_json(self, filename, data):
+        with open(filename, encoding='utf-8', mode="w") as f:
+            json.dump(data, f, indent=4, sort_keys=True,
+                      separators=(',', ' : '))
+        return data
+
+
+dataIO = DataIO()
