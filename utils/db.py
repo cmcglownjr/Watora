d9465953d109e533f2faf88ea268afb609062e96
Justin Garringer <49808342+soulnull@users.noreply.github.com>
2024-03-06 15:42:59 -0500

    Revert "improve"
    This reverts commit 8410363923a813dfd1f972c7e892c8739974c0f2.

--- utils/db.py
+++ utils/db.py
@@ -1,82 +1,115 @@
-import dataclasses
-import asyncio
 import motor.motor_asyncio
-from typing import Any, Dict, Final, Optional
+
 from abc import ABC
+from utils.watora import globprefix, def_time, def_v, def_vote, db_host, db_port
+
 
 class Settings(ABC):
-    id: int
+    def __init__(self, id_):
+        self._id = id_
+
 
 class GuildSettings(Settings):
-    prefix: str = dataclasses.field(default=globprefix)
-    language: str = dataclasses.field(default="english")
-    volume: float = dataclasses.field(default=def_v)
-    vote: int = dataclasses.field(default=def_vote)
-    timer: int = dataclasses.field(default=def_time)
-    autoplay: bool = dataclasses.field(default=True)
-    owo: bool = dataclasses.field(default=False)
-    lazy: bool = dataclasses.field(default=False)
-    channel: Optional[int] = dataclasses.field(default=None)
-    bound: Optional[int] = dataclasses.field(default=None)
-    customcommands: Dict[str, str] = dataclasses.field(default_factory=dict)
-    points: Dict[str, int] = dataclasses.field(default_factory=dict)
-    welcomes: Dict[str, str] = dataclasses.field(default_factory=dict)
-    goodbyes: Dict[str, str] = dataclasses.field(default_factory=dict)
-    autosongs: Dict[str, str] = dataclasses.field(default_factory=dict)
-    djs: list = dataclasses.field(default_factory=list)
-    roles: list = dataclasses.field(default_factory=list)
-    autoroles: list = dataclasses.field(default_factory=list)
-    disabledchannels: Dict[str, bool] = dataclasses.field(default_factory=dict)
-    disabledcommands: list = dataclasses.field(default_factory=list)
-    blacklisted: list = dataclasses.field(default_factory=list)
-    clans: list = dataclasses.field(default_factory=list)
-    respect: int = dataclasses.field(default=0)
+    def __init__(self, id, **kwargs):
+        super().__init__(id)
+
+        self.prefix = kwargs.get("prefix", globprefix)  # default prefix
+        self.language = kwargs.get("language", "english")  # default language
+        self.volume = kwargs.get("volume", def_v)  # default volume
+        self.vote = kwargs.get("vote", def_vote)  # default vote percent
+        self.timer = kwargs.get("timer", def_time)  # default timer value
+        self.autoplay = kwargs.get("autoplay", True)
+        self.owo = kwargs.get("owo", False)
+        self.lazy = kwargs.get("lazy", False)
+        self.channel = kwargs.get("channel", None)
+        self.bound = kwargs.get("bound", None)
+        self.customcommands = kwargs.get("customcommands", {})
+        self.points = kwargs.get("points", {})
+        self.welcomes = kwargs.get("welcomes", {})
+        self.goodbyes = kwargs.get("goodbyes", {})
+        self.autosongs = kwargs.get("autosongs", {})
+        self.djs = kwargs.get("djs", [])
+        self.roles = kwargs.get("roles", [])
+        self.autoroles = kwargs.get("autoroles", [])
+        self.disabledchannels = kwargs.get("disabledchannels", {})
+        self.disabledcommands = kwargs.get("disabledcommands", [])
+        self.blacklisted = kwargs.get("blacklisted", [])
+        self.clans = kwargs.get("clans", [])
+        self.respect = kwargs.get("respect", 0)
+
 
 class GlobSettings(Settings):
-    blacklisted: list = dataclasses.field(default_factory=list)
-    votes: list = dataclasses.field(default_factory=list)
-    autoplaylists: Dict[str, list] = dataclasses.field(default_factory=dict)
-    marry: Dict[str, str] = dataclasses.field(default_factory=dict)
-    donation: Dict[str, str] = dataclasses.field(default_factory=dict)
-    claim: Dict[str, str] = dataclasses.field(default_factory=dict)
-    animes: Dict[str, str] = dataclasses.field(default_factory=dict)
-    cachedanimes: Dict[str, str] = dataclasses.field(default_factory=dict)
-    server_count: Dict[str, int] = dataclasses.field(default_factory=dict)
-    source: str = dataclasses.field(default='ytsearch')
+    def __init__(self, id, **kwargs):
+        super().__init__(id)
+        self.blacklisted = kwargs.get("blacklisted", [])
+        self.votes = kwargs.get("votes", [])
+        self.autoplaylists = kwargs.get("autoplaylists", {})
+        self.marry = kwargs.get("marry", {})
+        self.donation = kwargs.get("donation", {})
+        self.claim = kwargs.get("claim", {})
+        self.animes = kwargs.get("animes", {})
+        self.cachedanimes = kwargs.get("cachedanimes", {})
+        self.server_count = kwargs.get("server_count", {})
+        self.source = kwargs.get("source", 'ytsearch')
+
 
 class AutoplaylistSettings(Settings):
-    songs: list = dataclasses.field(default_factory=list)
-    name: str = dataclasses.field(default="")
-    created_by: str = dataclasses.field(default="")
-    created_by_name: str = dataclasses.field(default="")
-    created_date: str = dataclasses.field(default="")
-    private: bool = dataclasses.field(default=True)
-    shuffle: bool = dataclasses.field(default=True)
-    whitelist: list = dataclasses.field(default_factory=list)
-    is_personal: bool = dataclasses.field(default=False)
-    description: str = dataclasses.field(default="")
-    avatar: str = dataclasses.field(default="")
-    upvote: list = dataclasses.field(default_factory=list)
+    def __init__(self, id, **kwargs):
+        super().__init__(id)
 
-@dataclasses.dataclass
+        self.songs = kwargs.get("songs", [])
+        self.name = kwargs.get("name", "")
+        self.created_by = kwargs.get("created_by", "")
+        self.created_by_name = kwargs.get("created_by_name", "")
+        self.created_date = kwargs.get("created_date", "")
+        self.private = kwargs.get("private", True)
+        self.shuffle = kwargs.get("shuffle", True)
+        self.whitelist = kwargs.get("whitelist", [])
+        self.is_personal = kwargs.get("is_personnal", False)
+        self.description = kwargs.get("description", "")
+        self.avatar = kwargs.get("avatar", "")
+        self.upvote = kwargs.get("upvote", [])
+
+
 class SettingsDB:
-    client: Final[motor.motor_asyncio.AsyncIOMotorClient]
-    db: Final[motor.motor_asyncio.AsyncIOMotorDatabase]
-    guild_settings_collection: Final[motor.motor_asyncio.AsyncIOMotorCollection]
-    glob_settings_collection: Final[motor.motor_asyncio.AsyncIOMotorCollection]
-    autoplaylist_settings_collection: Final[motor.motor_asyncio.AsyncIOMotorCollection]
+    _instance = None
 
-    @classmethod
-    async def create(cls) -> "SettingsDB":
-        client = motor.motor_asyncio.AsyncIOMotorClient(db_host, db_port)
-        db = client.Watora
-        return cls(client, db, db.settings, db.glob, db.autoplaylists)
+    def __init__(self):
+        self.client = motor.motor_asyncio.AsyncIOMotorClient(db_host, db_port)
+        self.db = self.client.Watora
+        self.guild_settings_collection = self.db.settings
+        self.glob_settings_collection = self.db.glob
+        self.autoplaylist_settings_collection = self.db.autoplaylists
 
-    async def get_glob_settings(self) -> GlobSettings:
+    @staticmethod
+    def get_instance():
+        if not SettingsDB._instance:
+            SettingsDB._instance = SettingsDB()
+        return SettingsDB._instance
+
+    async def get_glob_settings(self):
         document = await self.glob_settings_collection.find_one({"_id": 0})
         if document:
-            return dataclasses.replace(GlobSettings(document.get("_id")), **document)
+            return GlobSettings(document.get("_id"), **document)
         return GlobSettings(0)
 
-    async def set_glob_settings(self, settings: GlobSettings) -> None:
-        await self.glob_settings_collection.replace_one({
+    async def set_glob_settings(self, settings):
+        return await self.glob_settings_collection.replace_one({"_id": 0}, settings.__dict__, True)
+
+    async def get_guild_settings(self, id):
+        document = await self.guild_settings_collection.find_one({"_id": id})
+        if document:
+            return GuildSettings(document.get("_id"), **document)
+        return GuildSettings(id)
+
+    async def set_guild_settings(self, settings):
+        return await self.guild_settings_collection.replace_one({"_id": settings._id}, settings.__dict__, True)
+
+    async def get_autoplaylists_settings(self, id):
+        document = await self.autoplaylist_settings_collection.find_one({"_id": id})
+        if document:
+            return AutoplaylistSettings(document.get("_id"), **document)
+        return AutoplaylistSettings(id)
+
+    async def set_autoplaylists_settings(self, settings):
+        return await self.autoplaylist_settings_collection.replace_one({"_id": settings._id}, settings.__dict__, True)
