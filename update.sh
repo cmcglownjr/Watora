a3aa5583628941765252f3cb6c20a77a7cc8ade8
Justin Garringer <49808342+soulnull@users.noreply.github.com>
2024-03-06 15:17:28 -0500

    improve

--- update.sh
+++ update.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-python3.7 -V > /dev/null 2>&1 || {
-    echo >&2 "Python 3.7 doesn't seem to be installed.  Do you have a weird installation?"
-    echo >&2 "If you have another python version up to 3.6+, use it to run run.py instead of this script."
-    exit 1; }
+# Check if Python 3.7 is installed
+if ! command -v python3.7 &> /dev/null
+then
+    echo "Python 3.7 doesn't seem to be installed.  Do you have a weird installation?"
+    echo "If you have another python version up to 3.6+, use it to run run.py instead of this script."
+    exit 1
+fi
 
-python3.7 -m pip install --upgrade -r requirements.txt
+# Check if pip is available for the installed Python version
+if ! python3.7 -m pip --version &> /dev/null
+then
+    echo "Pip for Python 3.7 doesn't seem to be installed. Installing it now."
+    curl -sS https://bootstrap.pypa.io/get-pip.py | python3.7
+fi
+
+# Upgrade pip and install requirements
+python3.7 -m pip install --upgrade pip
+python3.7 -m pip install -r requirements.txt
+
