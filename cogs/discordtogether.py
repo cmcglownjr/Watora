12c7dd489d6a71e20592b36a9947bff1a5c83afb
Justin Garringer <49808342+soulnull@users.noreply.github.com>
2024-03-06 15:17:30 -0500

    improve

--- cogs/discordtogether.py
+++ cogs/discordtogether.py
@@ -3,101 +3,42 @@ from discord.ext import commands
 from discordTogether import DiscordTogether
 from utils.watora import get_str, get_color
 
-class discordtogether(commands.Cog):
+class DiscordTogetherCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.togetherControl = DiscordTogether(bot)
 
-    @commands.command(aliases=['youtube'])
-    async def youtubetogether(self, ctx):
-        """
-            {command_prefix}youtube
-
-        Starts the youtube activity in your voice channel.
-        """
+    async def start_activity(self, ctx, activity_name, link_text):
         if not ctx.author.voice:
             return await ctx.send(get_str(ctx, "music-join-no-channel"))
-        link = await self.togetherControl.create_link(ctx.author.voice.channel.id, 'youtube')
+        link = await self.togetherControl.create_link(ctx.author.voice.channel.id, activity_name)
 
         embed=discord.Embed(
-            title="Youtube Together",
+            title=f"{activity_name.capitalize()} Together",
             description=f"[Click Here]({link})",
             color=get_color(ctx.guild)
         )
         await ctx.send(embed=embed)
+
+    @commands.command(aliases=['youtube'])
+    async def youtubetogether(self, ctx):
+        await self.start_activity(ctx, 'youtube', 'youtube')
 
     @commands.command(aliases=['poker'])
     async def pokertogether(self, ctx):
-        """
-            {command_prefix}poker
-
-        Starts the poker activity in your voice channel.
-        """
-        if not ctx.author.voice:
-            return await ctx.send(get_str(ctx, "music-join-no-channel"))
-        link = await self.togetherControl.create_link(ctx.author.voice.channel.id, 'poker')
-
-        embed=discord.Embed(
-            title="Poker Together",
-            description=f"[Click Here]({link})",
-            color=get_color(ctx.guild)
-        )
-        await ctx.send(embed=embed)
+        await self.start_activity(ctx, 'poker', 'poker')
 
     @commands.command(aliases=['chess'])
     async def chesstogether(self, ctx):
-        """
-            {command_prefix}chess
-
-        Starts the chess activity in your voice channel.
-        """
-        if not ctx.author.voice:
-            return await ctx.send(get_str(ctx, "music-join-no-channel"))
-        link = await self.togetherControl.create_link(ctx.author.voice.channel.id, 'chess')
-
-        embed=discord.Embed(
-            title="Chess Together",
-            description=f"[Click Here]({link})",
-            color=get_color(ctx.guild)
-        )
-        await ctx.send(embed=embed)
+        await self.start_activity(ctx, 'chess', 'chess')
 
     @commands.command(aliases=['betrayal'])
     async def betrayaltogether(self, ctx):
-        """
-            {command_prefix}betrayal
-
-        Starts the betrayal activity in your voice channel.
-        """
-        if not ctx.author.voice:
-            return await ctx.send(get_str(ctx, "music-join-no-channel"))
-        link = await self.togetherControl.create_link(ctx.author.voice.channel.id, 'betrayal')
-
-        embed=discord.Embed(
-            title="Betrayal Together",
-            description=f"[Click Here]({link})",
-            color=get_color(ctx.guild)
-        )
-        await ctx.send(embed=embed)
+        await self.start_activity(ctx, 'betrayal', 'betrayal')
 
     @commands.command(aliases=['fishing'])
     async def fishingtogether(self, ctx):
-        """
-            {command_prefix}fishing
-
-        Starts the fishing activity in your voice channel.
-        """
-        if not ctx.author.voice:
-            return await ctx.send(get_str(ctx, "music-join-no-channel"))
-        link = await self.togetherControl.create_link(ctx.author.voice.channel.id, 'fishing')
-
-        embed=discord.Embed(
-            title="Fishing Together",
-            description=f"[Click Here]({link})",
-            color=get_color(ctx.guild)
-            )
-        await ctx.send(embed=embed)
-
+        await self.start_activity(ctx, 'fishing', 'fishing')
 
 def setup(bot):
-    bot.add_cog(discordtogether(bot))
+    bot.add_cog(DiscordTogetherCog(bot))
